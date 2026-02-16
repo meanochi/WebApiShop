5d7ce5ffe8b6c74544d8739afba1a058053e33b8
rachel85891 <r0583285891@gmail.com>
2026-02-16 10:51:19 +0200

    add

--- Repositories/ShowsRepository.cs
+++ Repositories/ShowsRepository.cs
@@ -39,6 +39,49 @@ namespace Repositories
             await _context.SaveChangesAsync();
             return show;
         }
+
+        public async Task<(IEnumerable<Show> shows, int total)> getAllShows( string description, int? minPrice, int? maxPrice, int skip, int position,int[]? categoryId)
+        {
+            var query = _context.Shows.Where(show =>
+                        (description == null ? (true) : (show.Description.Contains(description)))
+                        && ((minPrice == null) ? (true) : (minShowPrice(show) >= minPrice))
+                        && ((maxPrice == null) ? (true) : (maxShowPrice(show) <= maxPrice))
+                        && (categoryId == null || categoryId.Length == 0 || categoryId.Contains(show.CategoryId)))
+                            .OrderBy(show => minShowPrice(show));
+
+            Console.WriteLine(query.ToQueryString());
+            List<Show> shows = await query.Skip((position - 1) * skip)
+            .Take(skip).Include(show => show.Category).ToListAsync();
+            var total = await query.CountAsync();
+            return (shows, total);
+        }
+
+        public int minShowPrice(Show show)
+        {
+            int minPrice = int.MaxValue;
+            foreach (var section in show.Sections)
+            {
+                if(section.Price < minPrice)
+                {
+                    minPrice = (int)section.Price;
+                }
+            }
+            return minPrice;
+        }
+
+        public int maxShowPrice(Show show)
+        {
+            int maxPrice = int.MinValue;
+            foreach (var section in show.Sections)
+            {
+                if (section.Price < maxPrice)
+                {
+                    maxPrice = (int)section.Price;
+                }
+            }
+            return maxPrice;
+        }
+
         //public async Task deleteShow(int id)
         //{
         //        await _context.Shows.ExecuteDeleteAsync(s => s.Id == id);
