f19317b4d0efcb5ffb39aa6ccdc40226db150bd1
meanochi <michal.icecream@gmail.com>
2026-02-26 01:42:56 +0200

    include

--- WebApiShop/Controllers/OrderedSeatController.cs
+++ WebApiShop/Controllers/OrderedSeatController.cs
@@ -1,14 +1,24 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using DTOs;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Services;
 
 namespace WebApiShop.Controllers
 {
     public class OrderedSeatController : Controller
     {
-        // GET: OrderedSeatController
-        public ActionResult Index()
+
+        IOrderedSeatsService _service;
+
+        public OrderedSeatController(IOrderedSeatsService service)
         {
-            return View();
+            _service = service;
+        }
+        // GET: OrderedSeatController
+        [HttpGet("showid/{showId}")]
+        public Task<ActionResult<List<OrderedSeatReadDTO>>> getOrderedSeatsForShow(int showId)
+        {
+            List<OrderedSeatReadDTO> orderedSeats = 
         }
 
         // GET: OrderedSeatController/Details/5
